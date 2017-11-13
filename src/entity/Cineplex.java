3f6f2965c7b57af4b04f28626674d2b3c7bbdcb5
miltonpi <33518309+miltonpi@users.noreply.github.com>
2017-11-14 02:58:52 +0800

    Update Cineplex.java

--- src/entity/Cineplex.java
+++ src/entity/Cineplex.java
@@ -3,27 +3,50 @@ package entity;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ * 
+ * @author Group5
+ *
+ */
+
 public class Cineplex implements Serializable{
+	
+	/** Cineplex Id */
     private int cineplexId;
+    /** Cineplex Name */
     private String name;
 
+    /* ******************** Constructors *********************/
     public Cineplex(int cineplexId, String name) {
         this.cineplexId = cineplexId;
         this.name = name;
     }
 
+    /* ******************** Getter and Setter Methods *********/
+    /**
+	 * @return the cineplexId
+	 */
     public int getCineplexId() {
         return cineplexId;
     }
 
+    /**
+	 * @return the name
+	 */
     public String getName() {
         return name;
     }
 
+    /**
+	 * @param cineplexId the cineplexId to set
+	 */
     public void setCineplexId(int cineplexId) {
         this.cineplexId = cineplexId;
     }
 
+    /**
+   	 * @param name the name to set
+   	 */
     public void setName(String name) {
         this.name = name;
     }
