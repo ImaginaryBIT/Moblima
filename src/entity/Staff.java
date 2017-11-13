9d1a3143f35e5aa5e5d44a6eb4ba20c76533ada3
ImaginaryBIT <32798440+ImaginaryBIT@users.noreply.github.com>
2017-11-13 16:25:11 +0800

    update

--- src/entity/Staff.java
+++ src/entity/Staff.java
@@ -1,26 +1,32 @@
 package entity;
 
 import java.util.Scanner;
+import java.util.GregorianCalendar;
 
-public class Staff {
+public class Staff 
+{
 	private int staffId;
+	private String username;	
 	private String password;
-	private String username;
 
-	public Staff(String username, String password) {
-		this.username = username;
-		this.password = password;
+	public Staff() 
+	{
+		this.username = "username";
+		this.password = "password";
 	}
 
-	public boolean login() {
-		return false;
+	public boolean login(String username, String password) 
+	{
+		if(this.username == username && this.password == password) return true;
+		else return false;
 	}
 
 	public void showStaffMenu() {
 		int choice;
 		Scanner sc = new Scanner(System.in);
 
-		do {
+		while(true)
+		{
 			System.out.println("=====================================");
 			System.out.println("|1. Configure System Settings       |");
 			System.out.println("|2. Add Movies                      |");
@@ -29,24 +35,12 @@ public class Staff {
 			System.out.println("=====================================");
 			System.out.print("Enter your choice: ");
 			choice = sc.nextInt();
-			sc.nextLine();
 
-			int configChoice;
-			switch (choice) {
+			switch (choice) 
+			{
 			case 1://Configure System Settings 
-				System.out.println("=====================================");
-				System.out.println("|1. Standard Ticket Price           |");
-				System.out.println("|2. Premium Ticket Price            |");
-				System.out.println("|3. Child Discount                  |");
-				System.out.println("|4. Senior Citizen Discount         |");
-				System.out.println("|5. Holiday Increment               |");
-				System.out.println("|6. Add Holiday                     |");
-				System.out.println("|7. Remove Holiday                  |");
-				System.out.println("=====================================");				
-				System.out.print("Enter your choice: ");
-				configChoice = sc.nextInt();
-				sc.nextLine();
-			break;
+				systemSettings();
+				break;
 			
 			case 2://Add Movies
 				System.out.print("Movie title: ");
@@ -57,8 +51,68 @@ public class Staff {
 				
 			case 3://Update movies
 				break;
+				
+			case 4://Logout
+				return;
+				
+			default:
+				System.out.print("Wrong input!");
+				break;
 			}		
-		} while (choice < 4 && choice > 0);
-
+		}
+	}
+		
+	
+	public void systemSettings()
+	{
+		int choiceSetting;
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) 
+		{
+			System.out.println("=====================================");
+			System.out.println("|1. Set Standard Ticket Price       |");
+			System.out.println("|2. Set Premium Ticket Price        |");
+			System.out.println("|3. Set Child Discount              |");
+			System.out.println("|4. Set Senior Citizen Discount     |");
+			System.out.println("|5. Holiday Increment               |");
+			System.out.println("|6. Add Holiday                     |");
+			System.out.println("|7. Remove Holiday                  |");
+			System.out.println("|8. Back to Main                    |");
+			System.out.println("=====================================");				
+			System.out.print("Enter your choice: ");
+			choiceSetting = sc.nextInt();
+			
+			switch (choiceSetting) 
+			{
+			case 1://Set Standard Ticket Price
+				break;
+			
+			case 2://Set Premium Ticket Price
+				break;
+				
+			case 3://Set Child Discount
+				break;
+				
+			case 4://Set Senior Citizen Discount
+				break;
+			
+			case 5://Holiday Increment
+				break;
+			
+			case 6://Add Holiday
+				break;
+				
+			case 7://Remove Holiday
+				break;
+				
+			case 8://Back to Main
+				return;	
+				
+			default:
+				System.out.print("Wrong input!");
+				break;			
+			}	
+		}	
 	}
 }
