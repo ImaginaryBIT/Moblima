48fdf95c2957aa894baaf1f6cc803e1b404f96ad
miltonpi <33518309+miltonpi@users.noreply.github.com>
2017-11-14 03:02:06 +0800

    Update Review.java

--- src/entity/Review.java
+++ src/entity/Review.java
@@ -1,10 +1,28 @@
 package entity;
 
+/**
+ * 
+ * @author Group5
+ *
+ */
+
 public class Review {
+	/** Review Id */
 	private int reviewId;
+	/** User Rating */
 	private int userRating;
+	/** Review Content*/
 	private String review;
+	/** MovieGoer */
 	private MovieGoer movieGoer;
+	
+	
+	/* ******************** Constructors *********************/
+	/**
+	 * @param reviewContent
+	 * @param userRating
+	 * @param movieGoer
+	 */
 
 	public Review(String reviewContent, int userRating, MovieGoer movieGoer) {
 		this.review = reviewContent;
@@ -12,13 +30,26 @@ public class Review {
 		this.movieGoer = movieGoer;
 	}
 
+	
+	/* ******************** Getter and Setter Methods *********/
+	/**
+	 * @return the review
+	 */
 	public String getContent() {
 		return review;
 	}
+	
+	/**
+	 * @return the userRating
+	 */
 	public int getUserRating()
 	{
 		return userRating;
 	}
+	
+	/**
+	 * @return the movieGoer
+	 */
 	public MovieGoer getMovieGoer()
 	{
 		return movieGoer;
