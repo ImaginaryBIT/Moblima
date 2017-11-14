18609d83c8d35cda0d87def86db8beecaa281295
thichbanla <thichbanla@gmail.com>
2017-11-15 06:25:43 +0800

    Add Transaction

--- src/entity/Transaction.java
+++ src/entity/Transaction.java
@@ -1,5 +1,58 @@
 package entity;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Transaction {
+	private String transactionId;
+	private ArrayList<Ticket> tickets;
+	private float totalPayment;
+	private String movieName;
+	private Date transactionDate;
+	private Date showTime;
+	private MovieGoer movieGoer;
+
+	public Transaction(Date showTime, String cinemaCode, String movieName, ArrayList<Ticket> tickets, MovieGoer movieGoer) {
+		setTxnId(showTime, cinemaCode);
+		this.tickets = tickets;
+		this.showTime = showTime;
+		this.movieName = movieName;
+		transactionDate = new Date();
+		this.movieGoer= movieGoer;
+	}
+
+	public String getTxnId() {
+		return transactionId;
+	}
+
+	public ArrayList<Ticket> getTickets() {
+		return tickets;
+	}
+
+	public float getTotalPayment() {
+		return totalPayment;
+	}
+
+	public String getMovieName() {
+		return movieName;
+	}
+
+	public Date getTxnDate() {
+		return transactionDate;
+	}
+
+	public Date getShowTime() {
+		return showTime;
+	}
+	public MovieGoer getMovieGoer()
+	{
+		return movieGoer;
+	}
+
+	private void setTxnId(Date dateTime, String cinemaCode) {
+		SimpleDateFormat dateFormatter = new SimpleDateFormat("YYYYMMDDHHMM");
+		this.transactionId = cinemaCode + dateFormatter.format(dateTime);
+	}
 
 }
