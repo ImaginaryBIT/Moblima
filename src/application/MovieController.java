d9fdbd694026304b323457ee4cd1988c870aa366
ImaginaryBIT <32798440+ImaginaryBIT@users.noreply.github.com>
2017-11-14 20:07:54 +0800

    update for checkDuplicateMovie

--- src/application/MovieController.java
+++ src/application/MovieController.java
@@ -21,9 +21,9 @@ public class MovieController {
 	private int movieId;
 	/** Movie Title */
 	private String title;//1
-	/** Movie Cast */
+	/** Movie Type 2D/3D */
 	private String movieType;//2
-	/** Movie Restrict Level */
+	/** Movie Cast */
 	private List<String> cast;//3
 	private String actor1;
 	private String actor2;
@@ -39,7 +39,7 @@ public class MovieController {
 	private float overallUserRate;//8
 	/** Movie Reviews */
 	private List<Review> reviews;//9
-	/** Movie Type ( ) */
+	/** Movie Restrict Level */
 	private String rating;//10
 	/** Show Times */
 	private List<ShowTime> showTimes;//11
@@ -59,132 +59,130 @@ public class MovieController {
 		boolean flag = false;
 		
 		try {
-			//0 movieId
-			movieId = size + 1;
-			
-			//1 title
-			System.out.print("Enter the new Movie Name: ");
-			title = sc.nextLine();
-			
-			//2 movieType
-			System.out.println("Choose the Movie Type: ");
-			System.out.println("1. G");
-			System.out.println("2. PG");
-			System.out.println("3. PG13 ");
-			System.out.println("4. NC16");
-			System.out.println("5. M18");
-			System.out.println("6. R21");
-			System.out.println("7. TBA");
-			System.out.print("Choice: ");
-			
-			do {
-				rchoice = sc.nextInt(); sc.nextLine();
-				switch(rchoice) {
-				case 1: rating = "G"; break;
-				case 2: rating = "PG"; break;
-				case 3: rating = "PG13"; break;
-				case 4: rating = "NC16"; break;
-				case 5: rating = "M18"; break;
-				case 6: rating = "R21"; break;
-				case 7: rating = "TBA"; break;
-				default: System.out.println("No such choice");			
+			while(true){
+				//0 movieId
+				movieId = size + 1;
+				
+				//1 title
+				System.out.print("Enter the new Movie Name: ");
+				title = sc.nextLine();
+				
+				//2 movieType
+				System.out.print("Enter the Movie type: ");
+				movieType = sc.nextLine();
+				
+				//4 director
+				System.out.print("Enter the Movie Director: ");
+				director = sc.nextLine();
+				
+				boolean errorflag = checkDuplicateMovie(title, director, movieType);
+				
+				if(!errorflag) {
+				
+					//3 cast
+					System.out.print("Enter the Movie Cast(First actor)");
+					actor1 = sc.nextLine();
+					
+					System.out.print("Enter the Movie Cast(second actor)");
+					actor2 = sc.nextLine();
+					
+					cast = new ArrayList<String>();
+					cast.add(actor1);
+					cast.add(actor2);
+					
+					//5 language
+					System.out.print("Enter the Movie language: ");
+					language = sc.nextLine();
+					
+					//6 synopsis
+					System.out.print("Enter the Movie synopsis: ");
+					synopsis = sc.nextLine();
+					
+					//7 runningTime
+					System.out.print("Enter the Movie running time: ");
+					runningTime = sc.nextInt();
+					
+					//8 overallUserRate
+					System.out.print("Enter the Movie overall user rate: ");
+					overallUserRate = sc.nextFloat();
+					
+					//9 reviews
+					reviews = null;
+					
+					//10 rating
+					System.out.println("Choose the Movie rating: ");
+					System.out.println("1. G");
+					System.out.println("2. PG");
+					System.out.println("3. PG13 ");
+					System.out.println("4. NC16");
+					System.out.println("5. M18");
+					System.out.println("6. R21");
+					System.out.println("7. TBA");
+					System.out.print("Choice: ");
+					
+					do {
+						rchoice = sc.nextInt(); sc.nextLine();
+						switch(rchoice) {
+						case 1: rating = "G"; break;
+						case 2: rating = "PG"; break;
+						case 3: rating = "PG13"; break;
+						case 4: rating = "NC16"; break;
+						case 5: rating = "M18"; break;
+						case 6: rating = "R21"; break;
+						case 7: rating = "TBA"; break;
+						default: System.out.println("No such choice");			
+						}
+					} while (rchoice < 1 || rchoice >7);	
+					
+					//11 showTimes need to continue
+					int showTimeId = showTimeList.size() + 1;
+					System.out.print("Enter the cinema name: ");
+					Cinema cinema;
+					System.out.print("Enter the show date and time in DD-MM-YYYY HH:MM: ");
+					Date showDateTime;
+					System.out.print("Enter the how many ticket: ");
+					Ticket[] tickets;
+					
+					//12 status
+					System.out.print("Select the Movie show status: ");
+					System.out.println("1. Coming Soon");
+					System.out.println("2. Preview");
+					System.out.println("3. Now Showing");
+					System.out.println("4. End of Showing");
+					System.out.print("Choice: ");
+					do {
+						choice = sc.nextInt();
+						switch(choice) {
+						case 1: status = "Coming Soon"; break;
+						case 2: status = "Preview"; break;
+						case 3: status = "Now Showing"; break;
+						case 4: status = "End of Showing"; break;
+						default: System.out.println("No such choice");			
+						}
+					} while (rchoice < 1 || rchoice >4);	
+					
+					Movie mov = new Movie(movieId, title, cast, director,
+							language, synopsis,runningTime,
+							overallUserRate, reviews, movieType,
+							rating, showTimes, status);
+					list.add(mov);
+					SerializeDB.writeSerializedObject("Movie.ser", list);
+					return true;
+					}
+					else {
+						System.out.println("Movie already in database.");
+						System.out.println("Choose 1 to Add another movie.");
+						System.out.println("Choose 2 to Back to menu.");
+						choice = sc.nextInt();
+						if(choice == 2)
+							return false;;
+						}
 				}
-			} while (rchoice < 1 || rchoice >7);	
-			
-			//3 cast
-			System.out.print("Enter the Movie Cast(First actor)");
-			actor1 = sc.nextLine();
-			
-			System.out.print("Enter the Movie Cast(second actor)");
-			actor2 = sc.nextLine();
-			
-			cast = new ArrayList<String>();
-			cast.add(actor1);
-			cast.add(actor2);
-			
-			//4 director
-			System.out.print("Enter the Movie Director: ");
-			director = sc.nextLine();
-			
-			//5 language
-			System.out.print("Enter the Movie language: ");
-			language = sc.nextLine();
-			
-			//6 synopsis
-			System.out.print("Enter the Movie synopsis: ");
-			synopsis = sc.nextLine();
-			
-			//7 runningTime
-			System.out.print("Enter the Movie running time: ");
-			runningTime = sc.nextInt();
-			
-			//8 overallUserRate
-			System.out.print("Enter the Movie overall user rate: ");
-			overallUserRate = sc.nextFloat();
-			
-			//9 reviews
-			reviews = null;
-			
-			//10 rating
-			System.out.print("Enter the Movie rating: ");
-			rating = sc.nextLine();
-			
-			//11 showTimes need to continue
-			int showTimeId = showTimeList.size() + 1;
-			System.out.print("Enter the cinema name: ");
-			Cinema cinema;
-			System.out.print("Enter the show date and time in DD-MM-YYYY HH:MM: ");
-			Date showDateTime;
-			System.out.print("Enter the how many ticket: ");
-			Ticket[] tickets;
-			
-			//12 status
-			System.out.print("Select the Movie show status: ");
-			System.out.println("1. Coming Soon");
-			System.out.println("2. Preview");
-			System.out.println("3. Now Showing");
-			System.out.println("4. End of Showing");
-			System.out.print("Choice: ");
-			do {
-				choice = sc.nextInt(); sc.nextLine();
-				switch(choice) {
-				case 1: status = "Coming Soon"; break;
-				case 2: status = "Preview"; break;
-				case 3: status = "Now Showing"; break;
-				case 4: status = "End of Showing"; break;
-				default: System.out.println("No such choice");			
-				}
-			} while (rchoice < 1 || rchoice >4);	
-			
-			
-			for(int i=0; i<list.size(); i++){
-				Movie movie = (Movie)list.get(i);
-
-				if(movie.getTitle().equals(title)) {
-					flag = true;
-					break;
-				}
-				else 
-					flag = false;
-			}
-			
-			if (!flag) { 
-				Movie mov = new Movie(movieId, title, cast, director,
-						language, synopsis,runningTime,
-						overallUserRate, reviews, movieType,
-						rating, showTimes, status);
-				list.add(mov);
-				SerializeDB.writeSerializedObject("Movie.ser", list);
-				return true;
-			}
-			else
-				System.out.println("Movie already in database. Update the details instead");
 		}
 		catch (Exception e ) {
 			System.out.println( "Exception >> " + e.getMessage() );
 			return false;
 		}
-		return false;
 	}
 
 	public boolean updateMovie() {
