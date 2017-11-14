859a2e4ebae6616b76f7447e20a201e03d3c9dd5
thichbanla <thichbanla@gmail.com>
2017-11-15 06:25:01 +0800

    add MovieGoer

--- src/entity/MovieGoer.java
+++ src/entity/MovieGoer.java
@@ -1,5 +1,61 @@
 package entity;
 
-public class MovieGoer {
+import java.util.ArrayList;
 
+public class MovieGoer {
+	private String movieGoerId;
+	private String name;
+	private String email;
+	private int contact;
+	private ArrayList<Transaction> txnList;
+	
+	public MovieGoer(String name, String email, int contact)
+	{
+		setMovieGoerId(name, contact);
+		this.name=name;
+		this.email=email;
+		this.contact=contact;
+	}
+	public String movieGoerId()
+	{
+		return movieGoerId;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public String email()
+	{
+		return email;
+	}
+	public int getContact()
+	{
+		return contact;
+	}
+	public ArrayList<Transaction> getMovieGoerTXN()
+	{
+		return txnList;
+	}
+	//set methods
+	public void setMovieGoerId(String name, int contact)
+	{
+		movieGoerId = name + String.valueOf(contact);
+	}
+	
+	public void setName(String name)
+	{
+		this.name=name;
+	}
+	public void setEmail(String email)
+	{
+		this.email=email;		
+	}
+	public void setContact(int contact)
+	{
+		this.contact=contact;
+	}
+public void setMovieGoerTXN(Transaction txn)
+{
+	this.txnList.add(txn);
+}
 }
