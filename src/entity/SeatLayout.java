de207e580acf0cc0a5468e4d38654132e18ab7e1
miltonpi <33518309+miltonpi@users.noreply.github.com>
2017-11-15 02:52:47 +0800

    add java comment

--- src/entity/SeatLayout.java
+++ src/entity/SeatLayout.java
@@ -1,11 +1,22 @@
 package entity;
 
-public class SeatLayout {
-	private int row;
-	private int col;
+/**
+ * This class layout the seat arrangement
+ * @author Group5
+ *
+ */
 
+public class SeatLayout {
+	/** Seat Layout row */
+	private int row;
+	/** Seat Layout column */
+	private int col;
+	/** Seat Layout Two column array */
 	private Seat[][] seats;
 
+	/**
+	 * Initializing of seat arrangement
+	 */
 	public SeatLayout() {
 		row = 10;
 		col = 10;
@@ -13,24 +24,47 @@ public class SeatLayout {
 		initializeSeat();
 	}
 
+	/**
+	 * Initialize based on row and column assigned
+	 * @param row
+	 * @param col
+	 */
 	public SeatLayout(int row, int col) {
 		this.row = row;
 		this.col = col;
 		initializeSeat();
 	}
 
+	/**
+	 * @param row
+	 * @param col
+	 * @return seats based on row and column assigned
+	 */
 	public Seat getSeat(int row, int col) {
 		return seats[row][col];
 	}
 
+	/**
+	 * 
+	 * @return the seats
+	 */
 	public Seat[][] getSeats() {
 		return seats;
 	}
 
+	/**
+	 * Set seat is available
+	 * @param row
+	 * @param col
+	 * @param available
+	 */
 	public void setSeats(int row, int col, boolean available) {
 		seats[row][col].setStatus(available);
 	}
 
+	/**
+	 * Create the seats
+	 */
 	private void initializeSeat() {
 		for (int i = 0; i < row; i++) {
 			for (int j = 0; j < col; j++) {
@@ -39,14 +73,24 @@ public class SeatLayout {
 		}
 	}
 
+	/**
+	 * @return the row
+	 */
 	public int getRow() {
 		return row;
 	}
 
+	/**
+	 * @return the column
+	 */
 	public int getCol() {
 		return col;
 	}
 
+	/**
+	 * get the avaliable seats
+	 * @return the count
+	 */
 	public int getAvailSeatNum() {
 		int count = 0;
 		for (int i = 0; i < 10; i++) {
@@ -59,6 +103,9 @@ public class SeatLayout {
 		return count;
 	}
 
+	/**
+	 * Printing Seat Layout
+	 */
 	public void printSeatLayout() {
 		System.out.print("  ");
 		   for (int index = 0; index < seats[0].length; index++) {
