bf79b85c4598329608a45ed8e47b57378a47bc6d
NYI NYI THWIN <thwin@cleargo.com>
2017-11-15 20:55:00 +0800

    seat entity update

--- src/entity/Seat.java
+++ src/entity/Seat.java
@@ -2,73 +2,28 @@ package entity;
 
 import java.io.Serializable;
 
-/**
- * 
- * @author Group5
- *
- */
-
 public class Seat implements Serializable{
-	//private int seatId;
-	
-	/** Seat Layout row */
-	private int row;
-	/** Seat Layout column */
-	private int column;
-	/** Status of Seat */
-	private boolean isAvailable = true;
-	
-	public Seat()
-	{}
+    private int seatId;
+    private int row;
+    private int column;
 
-	/* ******************** Constructors *********************/
-	
-	/**
-	 * This constructs the Seat with row-column layout and status
-	 * @param row
-	 * @param column
-	 * @param isAvailable
-	 */
-    public Seat(int row, int column, boolean isAvailable) {
-      //  this.seatId = seatId;
+    public Seat(int seatId, int row, int column) {
+        this.seatId = seatId;
         this.row = row;
         this.column = column;
-        this.isAvailable = isAvailable;
     }
-    
-    /* ******************** Getter and Setter Methods *********/
 
-  //  public int getSeatId() {
-    //    return seatId;
-    //}
+    public int getSeatId() {
+        return seatId;
+    }
 
-    /**
-     * @return the row
-     */
     public int getRow() {
         return row;
     }
 
-    /**
-     * @return the column
-     */
     public int getColumn() {
         return column;
     }
-	
-    /**
-     * @return the isAvailable
-     */
-    public boolean checkStatus()
-    {
-    	return isAvailable;
-    }
     
-    /**
-     * @param isAvailable the isAvailable to set
-     */
-    public void setStatus(boolean isAvailable)
-    {
-    	this.isAvailable = isAvailable;
-    }
+    
 }
