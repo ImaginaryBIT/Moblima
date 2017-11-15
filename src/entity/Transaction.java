717976a54afc32993ec5f56d033309450e183d30
htethtet <htethtetchit1193@gmail.com>
2017-11-15 09:30:11 +0800

    Update Transaction.java

--- src/entity/Transaction.java
+++ src/entity/Transaction.java
@@ -1,19 +1,19 @@
 package entity;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Transaction {
 	private String transactionId;
-	private ArrayList<Ticket> tickets;
+	private List<Ticket> tickets;
 	private float totalPayment;
 	private String movieName;
 	private Date transactionDate;
 	private Date showTime;
 	private MovieGoer movieGoer;
 
-	public Transaction(Date showTime, String cinemaCode, String movieName, ArrayList<Ticket> tickets, MovieGoer movieGoer) {
+	public Transaction(Date showTime, String cinemaCode, String movieName, List<Ticket> tickets, MovieGoer movieGoer) {
 		setTxnId(showTime, cinemaCode);
 		this.tickets = tickets;
 		this.showTime = showTime;
@@ -22,34 +22,134 @@ public class Transaction {
 		this.movieGoer= movieGoer;
 	}
 
-	public String getTxnId() {
+	
+	/**
+	 * @return the transactionId
+	 */
+	public String getTransactionId() {
 		return transactionId;
 	}
 
-	public ArrayList<Ticket> getTickets() {
+
+
+	/**
+	 * @return the tickets
+	 */
+	public List<Ticket> getTickets() {
 		return tickets;
 	}
 
+
+
+	/**
+	 * @return the totalPayment
+	 */
 	public float getTotalPayment() {
 		return totalPayment;
 	}
 
+
+
+	/**
+	 * @return the movieName
+	 */
 	public String getMovieName() {
 		return movieName;
 	}
 
-	public Date getTxnDate() {
+
+
+	/**
+	 * @return the transactionDate
+	 */
+	public Date getTransactionDate() {
 		return transactionDate;
 	}
 
+
+
+	/**
+	 * @return the showTime
+	 */
 	public Date getShowTime() {
 		return showTime;
 	}
-	public MovieGoer getMovieGoer()
-	{
+
+
+
+	/**
+	 * @return the movieGoer
+	 */
+	public MovieGoer getMovieGoer() {
 		return movieGoer;
 	}
 
+
+
+	/**
+	 * @param transactionId the transactionId to set
+	 */
+	public void setTransactionId(Date dateTime, String cinemaCode) {
+		SimpleDateFormat dateFormatter = new SimpleDateFormat("YYYYMMDDHHMM");
+		this.transactionId = cinemaCode + dateFormatter.format(dateTime);
+	}
+
+
+
+	/**
+	 * @param tickets the tickets to set
+	 */
+	public void setTickets(List<Ticket> tickets) {
+		this.tickets = tickets;
+	}
+
+
+
+	/**
+	 * @param totalPayment the totalPayment to set
+	 */
+	public void setTotalPayment(float totalPayment) {
+		this.totalPayment = totalPayment;
+	}
+
+
+
+	/**
+	 * @param movieName the movieName to set
+	 */
+	public void setMovieName(String movieName) {
+		this.movieName = movieName;
+	}
+
+
+
+	/**
+	 * @param transactionDate the transactionDate to set
+	 */
+	public void setTransactionDate(Date transactionDate) {
+		this.transactionDate = transactionDate;
+	}
+
+
+
+	/**
+	 * @param showTime the showTime to set
+	 */
+	public void setShowTime(Date showTime) {
+		this.showTime = showTime;
+	}
+
+
+
+	/**
+	 * @param movieGoer the movieGoer to set
+	 */
+	public void setMovieGoer(MovieGoer movieGoer) {
+		this.movieGoer = movieGoer;
+	}
+
+
+	// use setTransactionId method. will remove this method
 	private void setTxnId(Date dateTime, String cinemaCode) {
 		SimpleDateFormat dateFormatter = new SimpleDateFormat("YYYYMMDDHHMM");
 		this.transactionId = cinemaCode + dateFormatter.format(dateTime);
