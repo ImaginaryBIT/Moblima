c0cbcbfddef8d2273f4febb5cd312a5bf0fec96d
htethtet <htethtetchit1193@gmail.com>
2017-11-17 01:08:03 +0800

    Update CreateSystemSetting.java

--- src/testing/CreateSystemSetting.java
+++ src/testing/CreateSystemSetting.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import application.SystemSettingController;
 import database.SerializeDB;
 import entity.SystemSetting;
 
@@ -13,12 +12,12 @@ public class CreateSystemSetting {
 	public static void main(String[] args) {
 		/*SystemSettingController systemController = new SystemSettingController();
 		systemController.printMenu();*/
-		List<Date> holidays = new ArrayList<Date>();
-		holidays.add(SystemSettingController.convertDate("01-09-2017"));
+		List<Date> holidays = new ArrayList<>();
+		/*holidays.add(SystemSettingController.convertDate("01-09-2017"));
 		holidays.add(SystemSettingController.convertDate("18-10-2017"));
-		holidays.add(SystemSettingController.convertDate("25-12-2017"));
+		holidays.add(SystemSettingController.convertDate("25-12-2017"));*/
 		SystemSetting systemSetting = new SystemSetting((float)8.00, (float)10.00, (float)3.00, (float)5.00, (float)3.00, holidays,(float)4.00,(float)5.00,(float)2.00);
-		List<SystemSetting> systemSettings = new ArrayList<SystemSetting>();
+		List<SystemSetting> systemSettings = new ArrayList<>();
 		systemSettings.add(systemSetting);
 		SerializeDB.writeSerializedObject("SystemSetting.ser", systemSettings);
 		
