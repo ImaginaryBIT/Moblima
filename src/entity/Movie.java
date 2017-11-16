cde1539371ca4420ca6512c428465585a708eb0b
thichbanla <thichbanla@gmail.com>
2017-11-17 00:58:55 +0800

    Update javadoc

--- src/entity/Movie.java
+++ src/entity/Movie.java
@@ -6,10 +6,13 @@ import java.util.List;
 import utilities.Utilities;
 
 /**
+ * Represents a movie. Contains Review and ShowTime lists. Used to query a
+ * movie's show time and review
  * 
  * @author Group5
- * 
+ *
  */
+
 public class Movie implements Serializable {
 
 	/**
@@ -18,10 +21,10 @@ public class Movie implements Serializable {
 	 * with respect to serialization
 	 */
 	private static final long serialVersionUID = 1L;
-	/** type of Movies  */
-    public static final String BLOCKBUSTER = "Blockbuster";
-    public static final String THREED = "3D";
-    public static final String DIGITAL = "Digital";
+	/** type of Movies */
+	public static final String BLOCKBUSTER = "Blockbuster";
+	public static final String THREED = "3D";
+	public static final String DIGITAL = "Digital";
 	/** Movie Id */
 	private int movieId;
 	/** Movie Title */
@@ -30,13 +33,13 @@ public class Movie implements Serializable {
 	private List<String> cast;
 	/** Director of the Movie */
 	private String director;
-	/** Movie Language */
+	/** Language of this movie */
 	private String language;
 	/** Brief Summary of the Movie */
 	private String synopsis;
 	/** Movie Running Time (minute) */
 	private int runningTime;
-	/** Movie Reviews */
+	/** List of reviews for this movie */
 	private List<Review> reviews;
 	/** Movie Type ( ) */
 	private String movieType;
@@ -44,29 +47,30 @@ public class Movie implements Serializable {
 	private String rating;
 	/** Show Times */
 	private List<ShowTime> showTimes;
-	/** Status */
+	/** Showing status of this movie */
 	private String status;
 
 	/* ******************** Constructors ******************** */
 
 	/**
-	 * @param movieId
-	 * @param title
-	 * @param cast
-	 * @param director
-	 * @param language
-	 * @param synopsis
-	 * @param runningTime
-	 * @param overallUserRate
-	 * @param reviews
-	 * @param movieType
-	 * @param rating
-	 * @param showTimes
-	 * @param status
+	 * This constructs a movie object with following details
+	 * @param movieId The ID of this movie
+	 * @param title The movie's title
+	 * @param cast The list of casts
+	 * @param director The director 
+	 * @param language The language this movie is in
+	 * @param synopsis The brief summary of this movie
+	 * @param runningTime The running time in minute 
+	 * @param overallUserRate The overall user rating
+	 * @param reviews The list of reviews for this movie
+	 * @param movie Type The movie type
+	 * @param rating The restrict level
+	 * @param showTimes The list of show time
+	 * @param status The showing status of this movie
 	 */
-	public Movie(int movieId, String title, List<String> cast, String director,
-			String language, String synopsis, int runningTime,List<Review> reviews, String movieType,
-			String rating, List<ShowTime> showTimes, String status) {
+	public Movie(int movieId, String title, List<String> cast, String director, String language, String synopsis,
+			int runningTime, List<Review> reviews, String movieType, String rating, List<ShowTime> showTimes,
+			String status) {
 
 		this.movieId = movieId;
 		this.title = title;
@@ -88,6 +92,7 @@ public class Movie implements Serializable {
 
 	/* ******************** Getter and Setter Methods ******** */
 	/**
+	 * Gets the ID of this movie
 	 * @return the movieId
 	 */
 	public int getMovieId() {
@@ -95,6 +100,7 @@ public class Movie implements Serializable {
 	}
 
 	/**
+	 * Gets the title of this movie
 	 * @return the title
 	 */
 	public String getTitle() {
@@ -102,6 +108,7 @@ public class Movie implements Serializable {
 	}
 
 	/**
+	 * Gets the list of casts in this movie
 	 * @return the cast
 	 */
 	public List<String> getCast() {
@@ -109,6 +116,7 @@ public class Movie implements Serializable {
 	}
 
 	/**
+	 * Gets the director
 	 * @return the director
 	 */
 	public String getDirector() {
@@ -116,6 +124,7 @@ public class Movie implements Serializable {
 	}
 
 	/**
+	 * Gets the language this movie is in
 	 * @return the language
 	 */
 	public String getLanguage() {
@@ -123,6 +132,7 @@ public class Movie implements Serializable {
 	}
 
 	/**
+	 * Gets the brief summary of this movie
 	 * @return the synopsis
 	 */
 	public String getSynopsis() {
@@ -130,14 +140,15 @@ public class Movie implements Serializable {
 	}
 
 	/**
+	 * Gets the running time in minute
 	 * @return the runningTime
 	 */
 	public int getRunningTime() {
 		return runningTime;
 	}
 
-	
 	/**
+	 * Gets a list of all reviews
 	 * @return the reviews
 	 */
 	public List<Review> getReviews() {
@@ -145,6 +156,7 @@ public class Movie implements Serializable {
 	}
 
 	/**
+	 * Gets the type of this movie
 	 * @return the movieType
 	 */
 	public String getMovieType() {
@@ -152,6 +164,7 @@ public class Movie implements Serializable {
 	}
 
 	/**
+	 * Gets the movie's restrict level
 	 * @return the rating
 	 */
 	public String getRating() {
@@ -159,6 +172,7 @@ public class Movie implements Serializable {
 	}
 
 	/**
+	 * Gets the date and time this movie is shown
 	 * @return the showTimes
 	 */
 	public List<ShowTime> getShowTimes() {
@@ -166,6 +180,7 @@ public class Movie implements Serializable {
 	}
 
 	/**
+	 * Gets the showing status of this movie
 	 * @return the status
 	 */
 	public String getStatus() {
@@ -173,97 +188,96 @@ public class Movie implements Serializable {
 	}
 
 	/**
-	 * @param movieId
-	 *            the movieId to set
+	 * Changes the ID of this movie
+	 * @param movieId the movieId to set
 	 */
 	public void setMovieId(int movieId) {
 		this.movieId = movieId;
 	}
 
 	/**
-	 * @param title
-	 *            the title to set
+	 * Changes the title
+	 * @param title the title to set
 	 */
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
 	/**
-	 * @param cast
-	 *            the cast to set
+	 * Changes the list of casts
+	 * @param cast the cast to set
 	 */
 	public void setCast(List<String> cast) {
 		this.cast = cast;
 	}
 
 	/**
-	 * @param director
-	 *            the director to set
+	 * Changes the director's name
+	 * @param director the director to set
 	 */
 	public void setDirector(String director) {
 		this.director = director;
 	}
 
 	/**
-	 * @param language
-	 *            the language to set
+	 * Changes the language this movie is in
+	 * @param language the language to set
 	 */
 	public void setLanguage(String language) {
 		this.language = language;
 	}
 
 	/**
-	 * @param synopsis
-	 *            the synopsis to set
+	 * Changes the summary of the movie
+	 * @param synopsis the synopsis to set
 	 */
 	public void setSynopsis(String synopsis) {
 		this.synopsis = synopsis;
 	}
 
 	/**
-	 * @param runningTime
-	 *            the runningTime to set
+	 * Changes the running time in minute
+	 * @param runningTime the runningTime to set
 	 */
 	public void setRunningTime(int runningTime) {
 		this.runningTime = runningTime;
 	}
 
-	
 	/**
-	 * @param reviews
-	 *            the reviews to set
+	 * Changes the list of all reviews for this movie
+	 * @param reviews the reviews to set
 	 */
 	public void setReviews(List<Review> reviews) {
 		this.reviews = reviews;
 	}
 
 	/**
-	 * @param movieType
-	 *            the movieType to set
+	 * Changes the movie's type
+	 * @param movieType the movieType to set
 	 */
 	public void setMovieType(String movieType) {
 		this.movieType = movieType;
 	}
 
 	/**
-	 * @param rating
-	 *            the rating to set
+	 * Changes the restrict level
+	 * @param rating the rating to set
 	 */
 	public void setRating(String rating) {
 		this.rating = rating;
 	}
 
 	/**
-	 * @param showTimes
-	 *            the showTimes to set
+	 * Changes the list of show time
+	 * @param showTimes the showTimes to set
 	 */
 	public void setShowTimes(List<ShowTime> showTimes) {
 		this.showTimes = showTimes;
 	}
 
 	/**
-	 * @param status
-	 *            the status to set
+	 * Changes the showing status of this movie
+	 * @param status the status to set
 	 */
 	public void setStatus(String status) {
 		this.status = status;
@@ -271,8 +285,7 @@ public class Movie implements Serializable {
 
 	/* *******************Methods ********************** */
 	/**
-	 * Get the number of ticket sold
-	 * 
+	 * Get the number of ticket sold	
 	 * @return total number of ticket
 	 */
 	public int getTicketSold() {
@@ -287,23 +300,23 @@ public class Movie implements Serializable {
 	}
 
 	/**
-	 * Calculate Overall User Rating
-	 * 
-	 * @return rating
+     * This calculate the overall user rate 
+     * based on the rate that user submitted for this movie
+     * which is stored in the review list
+	 * @return the overallUserRate
 	 */
 	public float getOverallUserRating() {
-	
+
 		int total_rating = 0;
-		// loop through all review and sum total rating
+		// loop through all reivew and sum total rating
 		for (int i = 0; i < reviews.size(); i++) {
 			Review rv = (Review) reviews.get(i);
 			total_rating += rv.getUserRating();
 		}
-		// total rating divided by no is overall rating
-		if(reviews.isEmpty()) {
+		// total rating devided by no is overall rating
+		if (reviews.isEmpty()) {
 			return 0;
-		}
-		else {
+		} else {
 			return total_rating / reviews.size();
 		}
 	}
@@ -322,7 +335,6 @@ public class Movie implements Serializable {
 				System.out.print(cast + ", ");
 			}
 		}
-		System.out.println();
 		System.out.println("Director    : " + this.director);
 		System.out.println("Language    : " + this.language);
 		System.out.println("Type        : " + this.movieType);
@@ -337,25 +349,29 @@ public class Movie implements Serializable {
 
 	}
 	
-	public void showMovieDetailWithReview(){
+	/**
+    * This shows a movie with its detals along with reviews
+	 */
+	public void showMovieDetailWithReview() {
 		System.out.printf("%30s %10s%n", "Movie Name", "Movie Type");
 		System.out.printf("%30 %10s%n", this.title, this.movieType);
 		System.out.println("------------Reviews-------------");
-		
 
 		List<Review> reviewList = this.reviews;
 		for (Review review : reviewList) {
-			System.out.printf("Posted by: %50s Rate: %10d%n",
-					review.getUserName(), review.getUserRating());
+			System.out.printf("Posted by: %50s Rate: %10d%n", review.getUserName(), review.getUserRating());
 			System.out.printf("%150s%n", review.getContent());
 			System.out.println();
 		}
 	}
+
 	/**
-	 * @method add review to movie
+	 * @method This add new review to this movie
+	 * @param name Name of the reviewer
+	 * @param email Email of the reviewer
+	 * @param review The content of this new review
 	 */
-	public boolean addReview(String name, String email, float rating,
-			String review) {
+	public boolean addReview(String name, String email, float rating, String review) {
 		for (Review rv : reviews) {
 			if (rv.getUserEmail().equals(email)) {
 				return false;
