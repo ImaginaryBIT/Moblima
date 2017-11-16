0f1eb6e50cb5eef139873adcdfe164638d9b6de2
thichbanla <thichbanla@gmail.com>
2017-11-16 20:05:13 +0800

    javadoc

--- src/entity/Cineplex.java
+++ src/entity/Cineplex.java
@@ -4,53 +4,62 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
+ * Represents a Cineplex
  * 
  * @author Group5
- *
+ * @since 16/11/2017
  */
 
-public class Cineplex implements Serializable{
-	
+public class Cineplex implements Serializable {
+
 	/** Cineplex Id */
-    private int cineplexId;
-    /** Cineplex Name */
-    private String name;
+	private int cineplexId;
+	/** Cineplex Name */
+	private String name;
 
-    /* ******************** Constructors *********************/
-    public Cineplex(int cineplexId, String name) {
-        this.cineplexId = cineplexId;
-        this.name = name;
-    }
+	/*
+	 * ******************** Constructors ********************* 
+	 * This instantiates a cineplex with the cineplex name and ID
+	 * @param cineplexId This cineplex's ID	 * 
+	 * @param name This cineplex's name
+	 *
+	 */
+	public Cineplex(int cineplexId, String name) {
+		this.cineplexId = cineplexId;
+		this.name = name;
+	}
 
-    /* ******************** Getter and Setter Methods *********/
-    /**
+	/* ******************** Getter and Setter Methods *********/
+	/**
+	 * Gets the ID of the cineplex
 	 * @return the cineplexId
 	 */
-    public int getCineplexId() {
-        return cineplexId;
-    }
+	public int getCineplexId() {
+		return cineplexId;
+	}
 
-    /**
+	/**
+	 * Gets the cineplex's name
 	 * @return the name
 	 */
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    /**
-	 * @param cineplexId the cineplexId to set
+	/**
+	 * Changes this cineplex's ID
+	 * @param cineplexId The cineplexId to change to
 	 */
-    public void setCineplexId(int cineplexId) {
-        this.cineplexId = cineplexId;
-    }
+	public void setCineplexId(int cineplexId) {
+		this.cineplexId = cineplexId;
+	}
 
-    /**
-   	 * @param name the name to set
-   	 */
-    public void setName(String name) {
-        this.name = name;
-    }
+	/**
+	 * Changes the name of this cineplex
+	 * @param name The name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
 
-	
-	
 }
