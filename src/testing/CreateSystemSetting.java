db7c58f35b5cc80285a42c978be38befbff4546f
htethtet <htethtetchit1193@gmail.com>
2017-11-17 16:50:45 +0800

    Update CreateSystemSetting.java

--- src/testing/CreateSystemSetting.java
+++ src/testing/CreateSystemSetting.java
@@ -13,9 +13,9 @@ public class CreateSystemSetting {
 		/*SystemSettingController systemController = new SystemSettingController();
 		systemController.printMenu();*/
 		List<Date> holidays = new ArrayList<>();
-		/*holidays.add(SystemSettingController.convertDate("01-09-2017"));
+		holidays.add(SystemSettingController.convertDate("01-09-2017"));
 		holidays.add(SystemSettingController.convertDate("18-10-2017"));
-		holidays.add(SystemSettingController.convertDate("25-12-2017"));*/
+		holidays.add(SystemSettingController.convertDate("25-12-2017"));
 		SystemSetting systemSetting = new SystemSetting((float)8.00, (float)10.00, (float)3.00, (float)5.00, (float)3.00, holidays,(float)4.00,(float)5.00,(float)2.00);
 		List<SystemSetting> systemSettings = new ArrayList<>();
 		systemSettings.add(systemSetting);
